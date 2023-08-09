f30d53730e867d05bd125b73b188b96668d94dc5
remi <remi.nkjm@gmail.com>
2023-08-09 19:03:10 +0900

    ルートを定義する

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//「パスが/blogs・HTTPメソッドがGET」という組み合わせに対しては、
+// BlogController のindexアクションを実行するよう定義しています。
+Route::get('/blogs', [\App\Http\Controllers\BlogController::class, 'index']); // 追加
